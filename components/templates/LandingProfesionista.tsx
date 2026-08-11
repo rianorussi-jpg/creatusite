ab044dc0d43d3b5aae73615c2478ab096fa192ab
rianorussi-jpg <rianorussi@gmail.com>
2026-08-11 10:15:20 -0600

    Delete components/templates directory

--- components/templates/LandingProfesionista.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-import LandingFlexible from './LandingFlexible';
-export default function LandingProfesionista({ business }: { business: any; products?: any[] }) {
-  return <LandingFlexible business={business} preset="esencia" />;
-}
